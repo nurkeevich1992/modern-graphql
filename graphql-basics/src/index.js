d88360aeaded2782375f4dc76e3666d5811bc2c8
Tilekbek(Taylor) Kadyrov <tkadyrov@rcgdirect.com>
2019-11-01 16:10:51 -0500

    indexjs has been extended

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,29 +1,59 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+// Create an Add query that returns a float
+// Set up "add" to take a two arguments (a, b) which are required floats.
+// Have the resolver send back the sum of the two arguments
+
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        hello: String!
+        greeting(name: String, position: String): String!
+        add(a: Float!, b: Float!): Float!
+        me: User!
+        post: Post!
+    }
+
+    type User {
+        id: ID!
         name: String!
-        location: String!
-        bio: String!
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        isPublished: Boolean!
     }
 `;
 
 // Resolvers
 const resolvers = {
     Query: {
-        hello() {
-            return 'This is my first query!';
+        add(parent, args) {
+            return args.a + args.b;
         },
-        name() {
-            return 'Tilek Kadyrov';
+        greeting(parent, args) {
+            if (args.name && args.position) {
+                return `Hello ${args.name}! Your are my favoriate ${args.position}.`;
+            }
+            return 'Hello!';
         },
-        location() {
-            return 'Chicago';
+        me() {
+            return {
+                id: '234asdad',
+                name: 'Mike',
+                email: 'mike@example.com',
+            };
         },
-        bio() {
-            return 'Im from Kyrgyzstan';
+        post() {
+            return {
+                id: '123sfsdf',
+                title: 'Post Title',
+                body: 'This is post body',
+                isPublished: true,
+            };
         },
     },
 };
@@ -34,5 +64,6 @@ const server = new GraphQLServer({
 });
 
 server.start(() => {
+    // eslint-disable-next-line no-console
     console.log('The server is up!');
 });
