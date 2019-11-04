f206eab17c4b438882f02cffee70cd49274a290c
Tilekbek(Taylor) Kadyrov <tkadyrov@rcgdirect.com>
2019-11-04 16:38:11 -0600

    two way relation between posts and authors

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,79 +1,102 @@
 import { GraphQLServer } from 'graphql-yoga';
 
-// +++++++++++ sample datas ++++++++++++++
-const users = [
-    {
-        id: 1,
-        name: 'Andrew',
-        email: 'andrew@example.com',
-        age: 28,
-    },
-    {
-        id: 2,
-        name: 'Sarah',
-        email: 'sarah@example.com',
-    },
-    {
-        id: 3,
-        name: 'Mike',
-        email: 'mike@example.com',
-    },
-];
+// data
 
 const posts = [
     {
-        id: 10,
-        title: 'GraphQL 101',
-        body: 'This is how to use GraphQL...',
+        id: 11,
+        title: 'Bishkek city',
+        body: 'Raining in the city of Bishkek',
         isPublished: true,
         author: 1,
     },
     {
-        id: 2,
-        title: 'GraphQL 201',
-        body: 'This is the advanced GraphQL post...',
-        isPublished: false,
-        author: 1,
+        id: 12,
+        title: 'Astana city',
+        body: 'Raining in the city of Astana',
+        isPublished: true,
+        author: 3,
     },
     {
-        id: 3,
-        title: 'Programming Music',
-        body: '',
+        id: 13,
+        title: 'Osh city',
+        body: 'Raining in the city of Osh',
         isPublished: true,
         author: 2,
     },
+    {
+        id: 14,
+        title: 'Almata city',
+        body: 'Raining in the city of Almata',
+        isPublished: true,
+        author: 3,
+    },
 ];
 
-// +++++++++++++++++++++++++++++++++++
+const authors = [
+    {
+        id: 1,
+        name: 'Tilekbek Kadyrov',
+        email: 'tilekbek@example.com',
+        age: 28,
+        posts: [11],
+    },
+    {
+        id: 2,
+        name: 'Bermet Kalbotoeva',
+        email: 'bermet@example.com',
+        posts: [12, 14],
+    },
+    {
+        id: 3,
+        name: 'Emin Nurke',
+        email: 'emin@example.com',
+        age: 4,
+        posts: [13],
+    },
+    {
+        id: 4,
+        name: 'Safia Nurke',
+        email: 'safia@example.com',
+    },
+];
 
-//  Schemas
+// schemas
 const typeDefs = `
     type Query {
-        users(query: String): [User!]!
-        posts(query: String): [Post!]!
-        me: User!
-        post: Post!
+        authors(query: String): [Author]!
+        posts(query: String): [Post]!
     }
 
-    type User {
+    type Author {
         id: ID!
         name: String!
         email: String!
         age: Int
+        posts: [Post]
     }
 
     type Post {
-        id: ID! 
+        id: ID!
         title: String!
-        body: String!
-        isPublished: Boolean!
-        author: User!
+        body: String
+        isPublished: Boolean
+        author: Author!
     }
 `;
 
-// Resolvers
+// resolvers
 const resolvers = {
     Query: {
+        // eslint-disable-next-line no-unused-vars
+        authors(parent, args, ctx, info) {
+            if (!args.query) {
+                return authors;
+            }
+
+            return authors.filter(author => author.name.toLowerCase().includes(args.query.toLowerCase()));
+        },
+        // eslint-disable-next-line no-unused-vars
         posts(parent, args, ctx, info) {
             if (!args.query) {
                 return posts;
@@ -85,36 +108,17 @@ const resolvers = {
                 return isTitleMatch || isBodyMatch;
             });
         },
-
-        users(parent, args, ctx, info) {
-            if (!args.query) {
-                return users;
-            }
-
-            return users.filter(user => user.name.toLowerCase().includes(args.query.toLowerCase()));
-        },
-
-        me() {
-            return {
-                id: '234asdad',
-                name: 'Mike',
-                email: 'mike@example.com',
-            };
-        },
-
-        post() {
-            return {
-                id: '123sfsdf',
-                title: 'Post Title',
-                body: 'This is post body',
-                isPublished: true,
-            };
-        },
     },
 
     Post: {
         author(parent, args, ctx, info) {
-            return users.find(user => user.id === parent.author);
+            return authors.find(author => author.id === parent.author);
+        },
+    },
+
+    Author: {
+        posts(parent, args, ctx, info) {
+            return posts.filter(post => post.author === parent.id);
         },
     },
 };
@@ -126,5 +130,5 @@ const server = new GraphQLServer({
 
 server.start(() => {
     // eslint-disable-next-line no-console
-    console.log('The server is up!');
+    console.log('server started');
 });
