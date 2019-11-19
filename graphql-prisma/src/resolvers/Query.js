e53a1be45ed8bd2133423bddf67c782e5fc7eac8
K-Bermet <bkalbotoeva@gmail.com>
2019-11-19 13:31:11 -0600

    updatedAt and createdAt

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -6,6 +6,7 @@ const Query = {
             first: args.first,
             skip: args.skip,
             after: args.after,
+            orderBy: args.orderBy,
         };
         if (args.query) {
             opArgs.where = {
@@ -28,6 +29,7 @@ const Query = {
             first: args.first,
             skip: args.skip,
             after: args.after,
+            orderBy: args.orderBy,
         };
 
         if (args.query) {
@@ -49,6 +51,7 @@ const Query = {
             first: args.first,
             skip: args.skip,
             after: args.after,
+            orderBy: args.orderBy,
         };
 
         if (args.query) {
@@ -69,6 +72,7 @@ const Query = {
         const opArgs = {
             first: args.first,
             skip: args.skip,
+            orderBy: args.orderBy,
         };
 
         return prisma.query.comments(opArgs, info);
